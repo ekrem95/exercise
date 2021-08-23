f098fe81f6beb039d6c887a38e76471de63b24c9
Ekrem Karatas <ekremk95@gmail.com>
2021-08-23 18:59:43 +0300

    first missing positive

---
+++ leetcode/First Missing Positive.go
@@ -0,0 +1,34 @@
+import (
+	"sort"
+)
+
+func firstMissingPositive(nums []int) int {
+	sort.Ints(nums)
+
+	if nums[0] > 1 || nums[len(nums)-1] < 1 {
+		return 1
+	}
+
+	var k int
+	for i := range nums {
+		if nums[i] > 0 {
+			k = i
+			break
+		}
+	}
+
+	max := nums[k]
+	if max != 1 && max-nums[k-1] > 1 && nums[k-1] < 1 {
+		return 1
+	}
+
+	for _, n := range nums[k+1:] {
+		diff := n - max
+		if diff > 1 {
+			return max + 1
+		}
+		max = n
+	}
+
+	return max + 1
+}
