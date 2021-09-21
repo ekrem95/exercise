f0368dd2611110ef7c1c44d44880edb5d545148f
Ekrem <ekremk95@gmail.com>
2021-09-21 19:51:24 +0300

    Remove Nth Node From End of List

---
+++ leetcode/Remove Nth Node From End of List.go
@@ -0,0 +1,27 @@
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func removeNthFromEnd(head *ListNode, n int) *ListNode {
+	if head.Next == nil {
+		return nil
+	}
+
+	nodes := make([]*ListNode, 0, 30)
+	for tmp := head; tmp.Next != nil; tmp = tmp.Next {
+		nodes = append(nodes, tmp)
+	}
+	nodes = append(nodes, nodes[len(nodes)-1].Next)
+
+	if len(nodes) == n {
+		return head.Next
+	}
+
+	sel := nodes[len(nodes)-1-n]
+	sel.Next = sel.Next.Next
+
+	return head
+}
