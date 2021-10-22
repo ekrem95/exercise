ba8ec5af3bfef27aa3af0456ecffae43ad414a96
Ekrem Karatas <ekremk95@gmail.com>
2021-10-22 11:45:19 +0300

    Valid Number

---
+++ leetcode/Valid Number.go
@@ -0,0 +1,11 @@
+import "regexp"
+
+var (
+	reDecimal = `[+-]?((\d+\.)|(\d+\.\d+)|(\.\d+))`
+	reInt     = `[+-]?\d+`
+	reNum     = regexp.MustCompile(`^((` + reDecimal + `)|(` + reInt + `))([eE]` + reInt + `)?$`)
+)
+
+func isNumber(s string) bool {
+	return reNum.MatchString(s)
+}
